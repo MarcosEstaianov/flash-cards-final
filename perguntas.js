62f1ac99d3968108c41b735889df442e405ca797
MarcosEstaianov <estaianov.marcos@escola.pr.gov.br>
2025-06-05 13:40:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Anime',
+    'Quem é o pai de Rudeus Greyhat?',
+    'Poul Greyrat'
+)
+
+criaCartao(
+    'Anime',
+    'Quem é a mãe de Rudeus Greyrat?',
+    'Zenit Greyrat'
+)
+
+criaCartao(
+    'Anime',
+    'Quais são as três esposas de Rudeus Greyhat?',
+    'Roxy, Eris e Shylphiette'
+)
+
+criaCartao(
+    'Anime',
+    'Com quantos anos Rudeus virou mago da água em nível avançado?',
+    'Aos 13 anos'
+)
